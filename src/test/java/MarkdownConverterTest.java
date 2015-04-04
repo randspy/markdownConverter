4331eeca27d0c40fdc9acab62752d255bdee453e
Przemyslaw Koziel <mos.koziel@gmail.com>
2015-04-04 17:24:51 +0200

    Markdown converter, lets get started

---
+++ src/test/java/MarkdownConverterTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MarkdownConverterTest {
+
+    @Test
+    public void
+    nothing() {
+        assertEquals(1, 1);
+    }
+}
